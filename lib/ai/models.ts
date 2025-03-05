0ae4b5a1f53b51ee0d968e9d7c06fb12b77a1382
devanshsinghmadvit <devansh@madvit.com>
2025-03-05 22:51:03 +0530

    t

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -12,10 +12,7 @@ export const myProvider = customProvider({
   languageModels: {
     'chat-model-small': openai('gpt-4o-mini'),
     'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
-    }),
+   
     'title-model': openai('gpt-4-turbo'),
     'artifact-model': openai('gpt-4o-mini'),
   },
